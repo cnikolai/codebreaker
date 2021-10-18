60b7ad23e2a661afaa73d67acabf1511896daa3a
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-18 19:09:30 -0400

    added game, guess, and webserviceproxy to project

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/WebServiceProxy.java
@@ -0,0 +1,52 @@
+package edu.cnm.deepdive.codebreaker.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.model.Guess;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface WebServiceProxy {
+
+  // What is the path that we are going to send this? Note: this is relative to base url from retrofit below
+  @POST("codes")
+  Call<Game> startGame(@Body Game game); //send game request in body of request.
+
+  @POST("codes/{gameId}/guesses")
+  Call<Guess> submitGuess(@Body Guess guess, @Path("gameId") String gameId); //name of placeholder where gameId goes in the path
+
+  static WebServiceProxy getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  class InstanceHolder {
+
+    private static final WebServiceProxy INSTANCE;
+
+    static {
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ") //to GSON: when you recieve a data-time, format it, when you send it, send it this way
+          .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
+      Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl("https://ddc-java.services/codebreaker/")
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .client(client)
+          .build();
+      INSTANCE = retrofit.create(WebServiceProxy.class);
+    }
+  }
+}
