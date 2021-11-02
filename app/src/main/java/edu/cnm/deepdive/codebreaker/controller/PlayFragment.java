9bd8d84e0b63899f1a413e5cb1246f4d9a825a0c
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-02 17:15:27 -0400

    Replaced Toast with Snackbar

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/PlayFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/PlayFragment.java
@@ -8,12 +8,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.snackbar.Snackbar;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GuessItemAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentPlayBinding;
@@ -49,14 +51,10 @@ public class PlayFragment extends Fragment implements InputFilter {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    //noinspection ConstantConditions
     viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
     getLifecycle().addObserver(viewModel);
-    viewModel.getThrowable().observe(getViewLifecycleOwner(), (throwable) -> {
-          if (throwable != null) {
-            Toast.makeText(getContext(), throwable.getMessage(), Toast.LENGTH_LONG).show();
-          }
-        }
-    );//gets access to livedata from viewmodel - just observing, run contents of bucket once this object changes.
+    viewModel.getThrowable().observe(getViewLifecycleOwner(), this::displayError);
     viewModel.getGame().observe(getViewLifecycleOwner(), this::update); //observes a game
   } //when fragment dies, then cleans up
 
@@ -119,5 +117,16 @@ public class PlayFragment extends Fragment implements InputFilter {
 
   private void checkSubmitConditions(int length) {
     binding.submit.setEnabled(length == codeLength);
+  }
+
+  private void displayError(Throwable throwable) {
+    if (throwable != null) {
+      Snackbar snackbar = Snackbar.make(binding.getRoot(),
+          getString(R.string.play_error_message, throwable.getMessage()),
+          Snackbar.LENGTH_INDEFINITE);
+      snackbar.setAction(R.string.error_dismiss,
+          (v) -> snackbar.dismiss());//gets access to livedata from viewmodel - just observing, run contents of bucket once this object changes.
+      snackbar.show();
+    }
   }
 }
