a27bd1ac3aa91fdda0a96f57d39a84c77cf05325
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-11 19:15:35 -0500

    GoogleSignInRepository completed.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInRepository.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInRepository.java
@@ -11,6 +11,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import edu.cnm.deepdive.codebreaker.BuildConfig;
 import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.SingleEmitter;
@@ -32,7 +33,7 @@ public class GoogleSignInRepository {
         .requestEmail()
         .requestId() //oauth key from google
         .requestProfile()
-//        .requestIdToken(BuildConfig.CLIENT_ID)//bearer token that we need
+        .requestIdToken(BuildConfig.CLIENT_ID)//bearer token that we need
         .build();
     client = GoogleSignIn.getClient(context, options);
   }
